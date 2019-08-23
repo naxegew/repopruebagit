14c0bf62890b9e707c879bf8247b348cc98d150d
alumno <alumno@LAB00PC00>
2019-08-22 21:10:46 -0300

    primera clase de programacion

---
+++ clase1/src/clase1.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : clase1.c
+ Author      : Ignacio
+ Version     :
+ Copyright   : clase1
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+int numero1;
+int numero2;
+int resultado;
+printf("\nnumero1: ");
+scanf("%d", &numero1);
+printf("\nnumero2: ");
+scanf("%d", &numero2);
+resultado = numero1 + numero2;
+printf("resultado:%d", resultado);
+return 0;
+}
